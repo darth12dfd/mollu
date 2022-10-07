caf5730b706f9a87687524e7e98d37227d17fcf5
Gyuwon <woni011420@gmail.com>
2022-10-07 11:51:25 +0900

    추가 버튼 모달 연결하기(2022년 10월 7일 TIL(개인공부))

--- src/components/main/Dashlet.jsx
+++ src/components/main/Dashlet.jsx
@@ -4,19 +4,34 @@ import Heading from '../../ui/Heading';
 import Button from '../../ui/Button';
 import Card from '../../ui/Card';
 import InlineList from '../../ui/InlineList';
+import Text from '../../ui/Text';
+
+import { Consumer as Modal } from '../../ui/Modal/context';
+import { ADD_STUDENT_MODAL } from '../../constants/modals';
 
 class Dashlet extends PureComponent {
   render() {
-    const { name } = this.props;
+    const { name, schoolLabel } = this.props;
     return (
-      <Card vertical={4} horizontal={4}>
-        <Heading level={4}>{name}</Heading>
-        <InlineList spacingBetween={1}>
-          <Button primary small>
-            추가
-          </Button>
-        </InlineList>
-      </Card>
+      <Modal>
+        {({ openModal }) => (
+          <Card vertical={4} horizontal={4}>
+            <Heading level={4}>
+              {name}
+              <Text>{schoolLabel}</Text>
+            </Heading>
+            <InlineList spacingBetween={1}>
+              <Button
+                primary
+                small
+                onPress={() => openModal(ADD_STUDENT_MODAL, { name, school: schoolLabel })}
+              >
+                추가
+              </Button>
+            </InlineList>
+          </Card>
+        )}
+      </Modal>
     );
   }
 }
